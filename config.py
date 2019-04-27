7f1164be5794d94281bed0a4aee2eadf105610e2
rehemafaith <rehemafaith01@gmail.com>
2019-04-27 12:15:29 +0300

    added the TestCofig for tests

--- config.py
+++ config.py
@@ -1,9 +1,19 @@
 import os
+
 class Config:
   '''
   General configuration parent class
   '''
-  SECRET_KEY ='fefe'
+  SECRET_KEY =os.environ.get("SECRET_KEY")
+  UPLOADED_PHOTOS_DEST ='app/static/photos'
+
+  MAIL_SERVER = 'smtp.googlemail.com'
+  MAIL_PORT = 587
+  MAIL_USE_TLS = True
+  MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+  MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
+
 class ProdConfig(Config):
   '''
   Production configuration child class
@@ -12,7 +22,7 @@ class ProdConfig(Config):
       Config:The parent configuration class with General configuration settings
 
   '''
-  pass
+  SQLALCHEMY_DATABASE_URI =os.environ.get("DATABASE_URL")
 
 class DevConfig(Config):
   '''
@@ -27,7 +37,15 @@ class DevConfig(Config):
 
   DEBUG = True 
 
+class TestConfig(Config):
+  '''
+This is the class which we will use to set the configurations during testing stage of the app
+    Args:
+        Config - this is the parent config class from which we inherit its properties
+
+  '''
 config_options ={
     'development':DevConfig,
-    'production': ProdConfig
+    'production': ProdConfig,
+    'test':TestConfig
   }
